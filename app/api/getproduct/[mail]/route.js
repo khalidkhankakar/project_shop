2253562e4aa7656ad5510226e470d13e911c4dce
Khalid kakar <126089249+khalidkhankakar@users.noreply.github.com>
2023-11-14 08:13:02 +0500

    Update route.js

--- app/api/getproduct/[mail]/route.js
+++ app/api/getproduct/[mail]/route.js
@@ -5,13 +5,9 @@ import { NextResponse } from "next/server";
 export async function GET(req,{params}) {
     await dbcon();
     try {
-        const userId = req.nextUrl.searchParams.get('mail')
 
-        console.log(userId)
-        console.log(params)
-        // khalidkakar13579@gmail.com
         const products = await Product.find({
-            creator:userId
+            creator:params.mail
         });
         
         return NextResponse.json({
