d52ecb1b721a14ac0c4284c2655201168313832b
Khalid kakar <126089249+khalidkhankakar@users.noreply.github.com>
2023-11-15 08:53:53 +0500

    Update2 Notlogin.jsx

--- components/Notlogin.jsx
+++ components/Notlogin.jsx
@@ -1,3 +1,4 @@
+'use client'
 import { signIn, useSession } from 'next-auth/react'
 import React from 'react'
 
@@ -8,8 +9,8 @@ const Notlogin = () => {
     <div className="flex flex-col items-center justify-center space-y-5 mt-4">
     {!session && <h1 className="font-bold mt-32s text-3xl">Create an account to Get started </h1>}
     {!session && <button className="hover:bg-blue-700 py-2   text-white bg-blue-500 px-6   rounded-lg text-xl" onClick={() => signIn()} >Signup</button>}
-    {!session && <p className="text-green-700 text-lg container m-auto">Inventory shop software is a powerful tool that simplifies the management of merchandise and product stock for businesses of all sizes. This software offers a comprehensive suite of features, enabling users to efficiently add, edit, create, and delete items from their inventory with ease. Its user-friendly interface streamlines the entire process, making it accessible even for those with minimal technical expertise.</p>}
-        {!session && <p className="text-blue-700 text-lg container m-auto">One standout feature of this inventory shop software is its robust security system, incorporating Google authentication for user accounts. This not only enhances the software's security but also provides users with a seamless and secure login experience. With Google authentication, businesses can trust that their sensitive inventory data remains protected from unauthorized access.</p>}
+    {!session && <p className="text-green-700 text-lg container w-[90vw] m-auto ">Inventory shop software is a powerful tool that simplifies the management of merchandise and product stock for businesses of all sizes. This software offers a comprehensive suite of features, enabling users to efficiently add, edit, create, and delete items from their inventory with ease. Its user-friendly interface streamlines the entire process, making it accessible even for those with minimal technical expertise.</p>}
+        {!session && <p className="text-blue-700 text-lg w-[90vw] container m-auto">One standout feature of this inventory shop software is its robust security system, incorporating Google authentication for user accounts. This not only enhances the software's security but also provides users with a seamless and secure login experience. With Google authentication, businesses can trust that their sensitive inventory data remains protected from unauthorized access.</p>}
     </div>
   )
 }
