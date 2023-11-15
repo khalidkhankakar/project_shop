58be130b6d9a0c0e42f8ec296bbda8383a7f1205
Khalid kakar <126089249+khalidkhankakar@users.noreply.github.com>
2023-11-15 09:20:39 +0500

    Update SearchBar.jsx

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -19,39 +19,38 @@ const SearchBar = () => {
 
 <div className='container m-auto w-[90vw] my-3'>
 
-    <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
     <div className="relative">
         <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-            <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+            <svg aria-hidden="true" className="w-5 h-5 text-gray-500 " fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
         </div>
-        <input type="search" onChange={(e)=>(setQuery(e.target.value))} id="default-search" className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 outline-none focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search for the Product"  />
+        <input type="search" onChange={(e)=>(setQuery(e.target.value))} id="default-search" className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-100 outline-none focus:ring-blue-500 focus:border-blue-500 " placeholder="Search for the Product"  />
     </div>
 
 
 
-{query && <div id="dropdown" className="z-10  bg-blue-50 divide-y divide-gray-100 rounded-lg shadow  dark:bg-gray-700">
-<ul className="flex justify-around items-center text-md font-bold py-2  text-black dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
+{query && <div id="dropdown" className="z-10  bg-blue-50 divide-y divide-gray-100 rounded-lg shadow  ">
+<ul className="flex justify-around items-center text-md font-bold py-2  text-black " aria-labelledby="dropdownDefaultButton">
    <li>
-     <p className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Product</p>
+     <p className="block px-4 py-2 hover:bg-gray-100 ">Product</p>
    </li>
    <li>
-     <p className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Quantity</p>
+     <p className="block px-4 py-2 hover:bg-gray-100e">Quantity</p>
    </li>
    <li>
-     <p className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Price</p>
+     <p className="block px-4 py-2 hover:bg-gray-100">Price</p>
    </li>
  </ul>
 
     { searchData.map((item)=>{
-   return <><ul className="flex justify-around  border-b-4 border-black items-center font-semiBold py-2  text-black dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
+   return <><ul className="flex justify-around  border-b-4 border-black items-center font-semiBold py-2  text-black " aria-labelledby="dropdownDefaultButton">
    <li>
-     <p className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">{item.title}</p>
+     <p className="block px-4 py-2 hover:bg-gray-100 ">{item.title}</p>
    </li>
    <li>
-     <p className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">{item.qty}</p>
+     <p className="block px-4 py-2 hover:bg-gray-100 ">{item.qty}</p>
    </li>
   <li className="flex space-x-1 items-center ">
-     <p className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">{item.price}</p>
+     <p className="block px-4 py-2 hover:bg-gray-100">{item.price}</p>
    </li>
  </ul>
  <hr className="border-2 border-black" /> </>   })
