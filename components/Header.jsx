67103c150a11fe0875aaca66c51f6ee818ba4df5
Khalid kakar <126089249+khalidkhankakar@users.noreply.github.com>
2023-11-15 08:54:22 +0500

    Update2 Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -11,10 +11,10 @@ const Header = () => {
   const router = useRouter();
   return (
 
-    <nav className="bg-white border-gray-200 dark:bg-gray-900">
-      <div className=" flex  items-center justify-between mx-auto p-4">
-        <Link href={'/'}><span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">InventryShop</span></Link>
-          <ul className="flex font-medium md:space-x-4 lg:space-x-5">
+    <nav className="bg-gray-700">
+      <div className=" flex  items-center md:px-3 px-1 justify-between mx-auto py-4">
+        <Link href={'/'}><span className="self-center text-xl md:text-2xl text-white font-semibold whitespace-nowrap">InventryShop</span></Link>
+          <ul className="flex items-center font-medium space-x-1 md:space-x-4 lg:space-x-5">
             <li >
               <Link href="/" className={` py-2  pl-2 pr-2 ${pathname === "/" ? "text-blue-400": "text-white"} rounded `} aria-current="page">Home</Link>
             </li>
@@ -24,7 +24,7 @@ const Header = () => {
             {session && <li className="cursor-pointer text-white hover:text-blue-400" onClick={() =>{
             signOut();
             router.push('/');}
-            }>SignOut</li>}
+            }>LogOut</li>}
 
             <li>{session && <Image src={session?.user?.image} width={40} height={40} alt="user" className="object-contain rounded-full " />}</li>
           </ul>
