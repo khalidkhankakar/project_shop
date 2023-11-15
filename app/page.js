90c586edca60e67dace15dc8052b821459b130dd
Khalid kakar <126089249+khalidkhankakar@users.noreply.github.com>
2023-11-15 08:53:21 +0500

    Update2 page.js

--- app/page.js
+++ app/page.js
@@ -1,8 +1,7 @@
-'use client'
 import Form2 from "@/components/Form2";
 import Notlogin from "@/components/Notlogin";
 
-export default async function Home() {
+export default function Home() {
 
   return (
     <>
